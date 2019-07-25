f1b4e8ab8ba5ca84f2a0969f8903d91df286929f
Tyler Beck <tbeck202@gmail.com>
2019-07-24 21:43:41 -0700

    Initial commit TwentyOne project

---
+++ TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Card
+    {
+        public Card() // CONSTRUCTOR SHOULD BE SET TO BE THE SAME NAME OF THE CLASS
+        {
+            Suit = "Spades";
+            face = "Two";
+        }
+        public string Suit { get; set; }
+        public string face { get; set; }
+    }
+}
