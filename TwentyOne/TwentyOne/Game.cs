b031fd185db2b2e7935d58e331e11388e49822ec
Tyler Beck <tbeck202@gmail.com>
2019-07-28 14:18:23 -0700

    update 21

---
+++ TwentyOne/TwentyOne/Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public abstract class Game
+    {
+        public List<string> Players { get; set; }
+        public string Name { get; set; }
+        public string Dealer { get; set; }
+
+        public abstract void Play();
+
+        public virtual void  ListPlayers()
+        {
+            foreach (string player in Players)
+            {
+                Console.WriteLine(player);
+            }
+        }
+    }
+}
---
+++ TwentyOne/TwentyOne/TwentyOneGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class TwentyOneGame : Game, IWalkAway
+    {
+        public override void Play()
+        {
+            Console.WriteLine("Game under construction...");
+        }
+        public override void ListPlayers()
+        {
+            Console.WriteLine("21 Players:");
+            base.ListPlayers();
+        }
+        public void WalkAway(Player player)
+        {
+            Console.WriteLine("Game under construction...");
+        }
+    }
+}
