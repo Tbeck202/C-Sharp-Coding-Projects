ce9a50208315ea9e2e2ea26efabc671afc20cc4e
Tyler Beck <tbeck202@gmail.com>
2019-08-17 17:08:52 -0700

    Update  InsuranceQuoteApp

--- InsuranceQuoteApp/InsuranceQuoteApp/Controllers/HomeController.cs
+++ InsuranceQuoteApp/InsuranceQuoteApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InsuranceQuoteApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,6 +23,41 @@ namespace InsuranceQuoteApp.Controllers
         public ActionResult Quote(
             string firstName, string lastName, string email, DateTime dob, int carYear, string carMake, string carModel, bool dui, int speedTick, bool coverage)
         {
+
+            Quote newQuote = new Quote();
+            newQuote.FirstName = firstName;
+            newQuote.LastName = lastName;
+            newQuote.Email = email;
+            newQuote.Dob = dob;
+            newQuote.CarYear = carYear;
+            newQuote.CarMake = carMake;
+            newQuote.CarModel = carModel;
+            newQuote.Dui = dui;
+            newQuote.SpeedTick = speedTick;
+            newQuote.Coverage = coverage;
+
+
+            using (InsuranceQuoteEntities db = new InsuranceQuoteEntities())
+            {
+                newQuote.TotalQuote = BuildQuote.Calculate(newQuote);
+
+                var quote = new Quote();
+                quote.FirstName = firstName;
+                quote.LastName = lastName;
+                quote.Email = email;
+                quote.Dob = dob;
+                quote.CarYear = carYear;
+                quote.CarMake = carMake;
+                quote.CarModel = carModel;
+                quote.Dui = dui;
+                quote.SpeedTick = speedTick;
+                quote.Coverage = coverage;
+                quote.TotalQuote = newQuote.TotalQuote;
+
+
+                db.Quotes.Add(quote);
+                db.SaveChanges();
+            }
             return View("~/Views/Home/Index");
         }
     }
---
+++ InsuranceQuoteApp/InsuranceQuoteApp/Models/BuildQuote.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceQuoteApp.Models
+{
+    public class BuildQuote
+    {
+        public static decimal Calculate(Quote quote)
+        {
+            decimal quoteTotal = 50;
+            DateTime quoteDay = DateTime.Now;
+            int userAge = Convert.ToInt32(quoteDay.Year - quote.Dob.Year);
+            if (userAge < 25 || userAge > 100)
+            {
+                quoteTotal += 25;
+            }
+            if (userAge < 18)
+            {
+                quoteTotal += 100;
+            }
+            if (quote.CarYear < 2000 || quote.CarYear > 2015)
+            {
+                quoteTotal += 25;
+            }
+            if (quote.CarMake.ToLower() == "porsche")
+            {
+                quoteTotal += 25;
+            }
+            if (quote.CarModel.ToLower() == "911 carrera")
+            {
+                quoteTotal += 25;
+            }
+            if (quote.SpeedTick > 0)
+            {
+                quoteTotal += quote.SpeedTick * 10;
+            }
+            if (quote.Dui == true)
+            {
+                quoteTotal = quoteTotal * 1.25m;
+            }
+            if (quote.Coverage == true)
+            {
+                quoteTotal = quoteTotal * 1.5m;
+            }
+             
+            return quoteTotal;
+        }
+    }
+}
